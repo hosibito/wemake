bf4c383f26b8e434c96453c36992129771dc2976
hosibito <hosibito@naver.com>
2025-05-18 16:53:36 +0900

    3.11 Loader Function (06:02)

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -13,13 +13,20 @@ export const meta: MetaFunction = () => {
     ];
 };
 
-export default function HomePage() {
+export const loader = () => {
+    console.log("home page loader");
+    return {
+        hello: "world",
+    };
+};
+
+export default function HomePage({ loaderData }) {
     return (
         <div className="px-20 space-y-40">
             <div className="grid grid-cols-3 gap-4">
                 <div>
                     <h2 className="text-5xl font-bold leading-tight tracking-tight">
-                        Today's Products
+                        Today's Products {JSON.stringify(loaderData)}
                     </h2>
                     <p className="text-xl font-light text-foreground">
                         The best products made by our community today.
