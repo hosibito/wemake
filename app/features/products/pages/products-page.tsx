8d0f6565cac4780891ddfa5660b98838e93b45bd
hosibito <hosibito@naver.com>
2025-05-18 17:17:46 +0900

    3.13 Redirects and Resource Routes (03:33)

--- app/features/products/pages/products-page.tsx
+++ app/features/products/pages/products-page.tsx
@@ -1,32 +1,9 @@
-import type { Route } from "./+types/products-page";
-import { Card, CardHeader, CardTitle, CardContent } from "~/common/components/ui/card";
+import { redirect } from "react-router";
 
-export function loader({ request }: Route.LoaderArgs) {
-    return {};
+export function loader() {
+    // return Response.json({
+    //     hello: "world",
+    // })
+    return redirect("/products/leaderboards")
 }
 
-export function action({ request }: Route.ActionArgs) {
-    return {};
-}
-
-export function meta(): ReturnType<Route.MetaFunction> {
-    return [
-        { title: "Products | wemake" },
-        { name: "description", content: "All products" },
-    ];
-}
-
-export default function ProductsPage() {
-    return (
-        <div className="p-8">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Products</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p>제품 목록 페이지입니다.</p>
-                </CardContent>
-            </Card>
-        </div>
-    );
-} 
