48ab3560b8ea2b0b5b432781a1435de11e57c61e
hosibito <hosibito@naver.com>
2025-05-18 13:51:15 +0900

    3.6 Post Card (11:53)

--- app/root.tsx
+++ app/root.tsx
@@ -27,7 +27,7 @@ export const links: Route.LinksFunction = () => [
 export function Layout({ children }: { children: React.ReactNode }) {
   // 우선은 dark 모드가 적용되게
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
