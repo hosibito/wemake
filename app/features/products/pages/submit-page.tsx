9fcc5aad0939cfffa97642331e572f5dfe3d733e
hosibito <hosibito@naver.com>
2025-09-25 15:04:47 +0900

    3.22 Inputs and Selects (19:15)

--- app/features/products/pages/submit-page.tsx
+++ app/features/products/pages/submit-page.tsx
@@ -1,12 +1,86 @@
-import type { MetaFunction } from "react-router";
+import { Hero } from "~/common/components/hero";
+import type { Route } from "./+types/submit-page";
+import { Form } from "react-router";
+import { Label } from "~/common/components/ui/label";
+import { Input } from "~/common/components/ui/input";
+import InputPair from "~/common/components/input-pair";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "~/common/components/ui/select";
+import SelectPair from "~/common/components/select-pair";
 
-export function loader() { return {}; }
-export function action() { return {}; }
-export const meta: MetaFunction = () => [
-    { title: "Submit Product | wemake" },
-    { name: "description", content: "Submit a new product" },
-];
+export const meta: Route.MetaFunction = () => {
+    return [
+        { title: "Submit Product | wemake" },
+        { name: "description", content: "Submit your product" },
+    ];
+};
 
-export default function SubmitPage() {
-    return <h1>Submit Product Page</h1>;
-} 
+export default function SubmitPage({ actionData }: Route.ComponentProps) {
+    return (
+        <div>
+            <Hero
+                title="Submit Your Product"
+                subtitle="Share your product with the world"
+            />
+            <Form className="grid grid-cols-2 gap-10 max-w-screen-lg mx-auto">
+                <div className="space-y-5">
+                    <InputPair
+                        label="Name"
+                        description="This is the name of your product"
+                        id="name"
+                        name="name"
+                        type="text"
+                        required
+                        placeholder="Name of your product"
+                    />
+                    <InputPair
+                        label="Tagline"
+                        description="60 characters or less"
+                        id="tagline"
+                        name="tagline"
+                        required
+                        type="text"
+                        placeholder="A concise description of your product"
+                    />
+                    <InputPair
+                        label="URL"
+                        description="The URL of your product"
+                        id="url"
+                        name="url"
+                        required
+                        type="url"
+                        placeholder="https://example.com"
+                    />
+                    <InputPair
+                        textArea
+                        label="Description"
+                        description="A detailed description of your product"
+                        id="description"
+                        name="description"
+                        required
+                        type="text"
+                        placeholder="A detailed description of your product"
+                    />
+                    <SelectPair
+                        label="Category"
+                        description="The category of your product"
+                        name="category"
+                        required
+                        placeholder="Select a category"
+                        options={[
+                            { label: "AI", value: "ai" },
+                            { label: "Design", value: "design" },
+                            { label: "Marketing", value: "marketing" },
+                            { label: "Development", value: "development" },
+                        ]}
+                    />
+                </div>
+            </Form>
+        </div>
+    );
+}
