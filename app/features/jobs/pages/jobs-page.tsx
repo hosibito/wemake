058e58c6c4de3a9ed81eecd07a31f1d578d8a827
hosibito <hosibito@naver.com>
2025-10-21 15:39:14 +0900

    3.48 Responsive Design (08:21) - 이전꺼 47임.

--- app/features/jobs/pages/jobs-page.tsx
+++ app/features/jobs/pages/jobs-page.tsx
@@ -22,8 +22,8 @@ export default function JobsPage() {
     return (
         <div className="space-y-20">
             <Hero title="Jobs" subtitle="Companies looking for makers" />
-            <div className="grid grid-cols-6 gap-20 items-start">
-                <div className="grid grid-cols-3 col-span-4 gap-5">
+            <div className="grid grid-cols-1 xl:grid-cols-6 gap-20 items-start">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:col-span-4 gap-5">
                     {Array.from({ length: 11 }).map((_, index) => (
                         <JobCard
                             key={`jobId-${index}`}
